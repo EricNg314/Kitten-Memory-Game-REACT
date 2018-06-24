45dfd2efc0dc41985ee5dba37651f7b2b0fdfa08
Okens <ng.eric314@gmail.com>
2018-06-24 13:22:51 -0700

    Updating with shuffle.

--- src/App.js
+++ src/App.js
@@ -20,32 +20,46 @@ class App extends Component {
 
   selectKitten = id => {
 
-    let kitten = this.state.kittens[id-1];
 
-    console.log(`id: ${id}`);
-    // console.log(kittens);
-    // console.log(kitten);
+    let kitten = this.state.kittens.filter(kitten => kitten.id === id)[0];
+    console.log("selected kitten", kitten);
+    // console.log(`id: ${id}`);
 
     if (kitten.selected == false) {
-      this.setState({ score: this.state.score +1});
-      // setTimeout(() => console.log(this.state.score), 100);
-     
-      kitten.selected = true;
+      // console.log("entered selected false")
+      this.state.score += 1;
+      // this.setState({ score: this.state.score + 1 });
+      setTimeout(() => console.log(this.state.score), 100);
       // console.log(kitten);
+
+      kitten.selected = true;
+
+      this.setState(this.shuffle(this.state.kittens));
+
     } else {
-      this.endGame();
+      this.setState(this.endGame());
+
     }
   };
 
   endGame = () => {
-    if(this.state.top_score < this.state.score){
-      this.setState({ top_score: this.state.score});
+    if (this.state.top_score < this.state.score) {
+      this.setState({ top_score: this.state.score });
     }
+    this.state.score = 0;
+    // this.state.kittens.
     console.log("End game.")
   }
 
-  shuffle = () => {
+  shuffle = array => {
     console.log("shuffled");
+
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+
   }
 
   render() {
@@ -54,15 +68,16 @@ class App extends Component {
 
       <Router>
         <div>
-          <Navbar score={this.state.score} top_score ={this.state.top_score}/>
+          <Navbar score={this.state.score} top_score={this.state.top_score} />
           <Jumbotron />
           <Wrapper>
-            <Route exact path="/" component={""} />
-            <div className="row mx-5">
-              {this.state.kittens.map(kitten => (
 
+            <div className="row mx-5">
+              {this.state.kittens.map((kitten, index) => (
                 <ImageCard
+                  key={index}
                   selectKitten={this.selectKitten}
+                  index={index}
                   id={kitten.id}
                   image={kitten.image}
                 />
